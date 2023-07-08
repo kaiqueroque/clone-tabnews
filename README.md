82319c69296ec327c2d65dbb8988da82feeb2c07
Kaique Roque <30600566+kaiqueroque@users.noreply.github.com>
2023-07-08 02:03:01 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do projeto tabnews.com.br sendo construído através do curso.dev do saudosismo @filipedeschamps.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Elisa, te amo muito mais que açaí! ❤️</h1>
+  return <h1>Elisa, te amo muito mais que açaí! ❤️</h1>;
 }
 
 export default Home;
